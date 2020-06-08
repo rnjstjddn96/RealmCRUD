8264a91c0b2d5ec58e931f43afcc1c6c2c594696
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-06-08 21:53:59 +0900

    Initial Commit

---
+++ RealmPrac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmPrac
+//
+//  Created by 권성우 on 2020/06/08.
+//  Copyright © 2020 권성우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
